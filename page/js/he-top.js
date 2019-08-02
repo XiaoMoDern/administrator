e62f6b821f095a1d2ca71b2e8854cfe8c3207153
xiao <xiano@139.com>
2019-08-02 11:23:44 +0800

    修改内容

---
+++ page/js/he-top.js
@@ -0,0 +1,7 @@
+$(function () {
+    $(".header-top").load("he-top.html", function () {
+        // console.log(666);
+        // console.log($('.header'));
+
+    });
+})
--- page/js/header.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    $(".header-top").load("header.html", function () {
-        console.log(666);
-    });
-})()
